6c9f17ec211f59d3a45b3162320ae8cae117b741
cshorg <colegrohs@outlook.com>
2024-10-03 17:37:54 -0700

    typo

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,9 +4,9 @@ import { MONGO_URI } from "../constants/env";
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(MONGO_URI)
-    console.log("Successfully connected to DB.")
+    console.log("Successfully connected to Database.")
   } catch (error) {
-    console.log("Could not connect to database", error)
+    console.log("Could not connect to Database", error)
     process.exit(1)
   }
 }
